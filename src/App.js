d63b3da49c47c7e965b9bfc8042c1ba12d4d7237
LoneWolf <saurabhdalakoti07@gmail.com>
2019-05-24 06:39:40 +0530

    particle theme added

--- src/App.js
+++ src/App.js
@@ -5,11 +5,34 @@ import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import './App.css';
 import 'tachyons';
+import Particles from 'react-particles-js';
+const particlesOptions={
+      "particles": {
+          "number": {
+              "value": 80
+          },
+          "size": {
+              "value": 3
+          }
+      },
+      "interactivity": {
+          "events": {
+              "onhover": {
+                  "enable": true,
+                  "mode": "repulse"
+              }
+          }
+      }
+  };
 
 class App extends Component{
   render(){
     return (
       <div className="App">
+      <Particles className='particles'
+      params={particlesOptions}
+      />
+
       <Navigation />
       <Logo />
       <Rank />

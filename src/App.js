574325839a524d8ad0fb45d750116646cc530231
LoneWolf <saurabhdalakoti07@gmail.com>
2019-05-24 11:36:00 +0530

    code cleaned and errors removed about for-> htmlFor and form error

--- src/App.js
+++ src/App.js
@@ -84,30 +84,31 @@ class App extends Component{
   onRouteChange=(route)=>{
     if(route==='signout')
       {this.setState({isSignedIn:false})}
-    else if(route=='home')
+    else if(route==='home')
       {this.setState({isSignedIn:true})}
     this.setState({route:route});
   }
 
   render(){
+    const {isSignedIn,imageUrl,route,box}=this.state;
     return (
       <div className="App">
       <Particles className='particles'
       params={particlesOptions}
       />
 
-      <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
       {
-      this.state.route==='home'?
+      route==='home'?
       <div>
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} 
         onButtonSubmit={this.onButtonSubmit}/>
-        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
+        <FaceRecognition box={box} imageUrl={imageUrl} />
       </div>
       :(
-          this.state.route==='signin'
+          route==='signin'
           ?<Signin onRouteChange={this.onRouteChange}/>
           :<Register onRouteChange={this.onRouteChange}/>
         )
@@ -115,6 +116,7 @@ class App extends Component{
       
       </div>
       );
+    }
   }
-}
+
 export default App;
